9be3800fde7344463fba03791bba533578ed1abe
Peter Hagström <petha443@gmail.com>
2014-12-09 09:09:36 +0100

    Adding code climate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
 require 'bundler/setup'
 Bundler.setup
 
-require 'simplecov'
 require "codeclimate-test-reporter"
-SimpleCov.add_filter 'vendor'
-SimpleCov.formatters = []
 
 require 'webmock/rspec'
 require 'kco_ruby'
@@ -12,8 +9,7 @@ require 'kco_ruby'
 WebMock.disable_net_connect!(:allow => "codeclimate.com")
 WebMock.disable_net_connect!(allow: %w{codeclimate.com})
 
-
-SimpleCov.start CodeClimate::TestReporter.configuration.profile
+CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
 
