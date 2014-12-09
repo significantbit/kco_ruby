6ae298ff29289474b8dd5158acd770d878ea98c1
Peter Hagström <petha443@gmail.com>
2014-12-09 09:12:36 +0100

    No source files problem for SimpleCov reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,13 @@ Bundler.setup
 require "codeclimate-test-reporter"
 
 require 'webmock/rspec'
-require 'kco_ruby'
 
 WebMock.disable_net_connect!(:allow => "codeclimate.com")
 WebMock.disable_net_connect!(allow: %w{codeclimate.com})
-
 CodeClimate::TestReporter.start
+
+require 'kco_ruby'
+
 
 RSpec.configure do |config|
 
